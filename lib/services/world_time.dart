cc3500f6eac2baf9f7642eac272115a201ba63db
Foluwa <moronfoluwaakintola@gmail.com>
2019-11-29 13:43:30 +0100

    I added Loaders / Spinners

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -3,36 +3,37 @@ import 'dart:convert';
 import 'package:intl/intl.dart';
 
 class WorldTime {
+
   String location; // location name for UI
   String time; // the time in that location
   String flag; // url to an asset flag icon
   String url; // location url for api endpoint
 
-  WorldTime({this.location, this.flag, this.url});
+  WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
-    try {
+
+    try{
       // make the request
-      Response response =
-          await get('http://worldtimeapi.org/api/timezone/$url');
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
       Map data = jsonDecode(response.body);
 
       // get properties from json
       String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1, 3);
+      String offset = data['utc_offset'].substring(1,3);
 
       // create DateTime object
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
 
-      print(now);
-
       // set the time property
       time = DateFormat.jm().format(now);
-// WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
-    } catch (e) {
-      print('Caught error: $e');
-      time = 'Could not get time data';
     }
+    catch (e) {
+      print(e);
+      time = 'could not get time';
+    }
+
   }
+
 }
