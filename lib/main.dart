e8d6a9c7efc7d464cdd9f286045c8356ce7bbbe8
MAC <mac@Foluwa.local>
2019-11-29 10:16:41 +0100

    I implemented flutter routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:time_app/pages/choose_location.dart';
 import 'package:time_app/pages/home.dart';
+import 'package:time_app/pages/loading.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+  initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation(),
+
+      },
     ));
